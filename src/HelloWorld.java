366a913d1b83adbfabd3c4abe7b9f0fbda9490f8
Beornus <s344080@oslomet.no>
2020-08-18 10:34:27 +0200

    Petra kan räkna till 20

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,13 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
 
+        System.out.println("Räkna till 10");
         for(int i=1; i<=10; i++){
+            System.out.print(i + " ");
+        }
+
+        System.out.println("Petra äknar till 20");
+        for(int i=11; i<=20; i++){
             System.out.print(i + " ");
         }
     }
