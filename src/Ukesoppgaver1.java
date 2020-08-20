7918d4f0ef946e1c095103145244fc521d5247b5
Beornus <s344080@oslomet.no>
2020-08-20 14:10:57 +0200

    Oppgave 1.1.4 ii + iii

--- src/Ukesoppgaver1.java
+++ src/Ukesoppgaver1.java
@@ -85,7 +85,7 @@ public class Ukesoppgaver1 {
     // i) 10, 5, 7, 2, 9, 1, 3, 8, 4, 6
     // ii) 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
-    public static int maks(int[] a)
+ /*   public static int maks(int[] a)
     {
         int m = 0;               // (i) 1 ) (ii) 1 )
         int maksverdi = a[0];    // (i) 2 ) (ii) 2 )
@@ -100,7 +100,42 @@ public class Ukesoppgaver1 {
 
         //Svar oppgave i) 42
         //Svar oppgave ii) 69
+    }
+  */
 
+    //1.Sje kk at Programkode 1.1.5 gir korrekte resultater. Hva skjer hvis a har lengde 1 og hva hvis a er tom (lengde 0).
+    // Se også Avsnitt 1.1.7.
+
+    public static void main(String[] args){
+        int [] a = {5, 6, 4, 23, 43, 13, 89};
+
+        int max = maks(a);
+        System.out.println(max);
     }
 
+    public static int maks(int[] a)  // versjon 3 av maks-metoden
+    {
+        int sist = a.length - 1;       // siste posisjon i tabellen
+        int m = 0;                     // indeks til største verdi
+        int maksverdi = a[0];          // største verdi
+        int temp = a[sist];            // tar vare på siste verdi
+        a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
+
+        for (int i = 0; ; i++)         // i starter med 0
+            if (a[i] >= maksverdi)       // denne blir sann til slutt
+            {
+                if (i == sist)             // sjekker om vi er ferdige
+                {
+                    a[sist] = temp;          // legger siste verdi tilbake
+                    return temp >= maksverdi ? sist : m;   // er siste størst?
+                }
+                else
+                {
+                    maksverdi = a[i];        // maksverdi oppdateres
+                    m = i;                   // m oppdateres
+                }
+            }
+    } // maks
+
+    // Ja koden gir korrekte resultater. Om arrayet har ett element blir svaret 0 och om arrayet är tomt får du ett exception.
 }
